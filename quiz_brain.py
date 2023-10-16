d8a5795d591c85ca2dab7215dd98e680ff9a2220
SaimNomani <nomanisaim591@gmail.com>
2023-10-16 13:15:02 +0500

    100DaysOfCode-day-17-Quiz-Game-version-1.1

--- quiz_brain.py
+++ quiz_brain.py
@@ -11,14 +11,12 @@ class QuizBrain:
         current_question=self.question_list[self.question_number]
         self.question_number+=1
         self.user_response=(input(f"Q.{self.question_number}: {current_question.text} (True/False): ")).title()
-        print(self.user_response)
         self.is_answer_correct(self.user_response, current_question.answer)
 
     def still_has_questions(self):
         return self.question_number <len(self.question_list)
 
     def is_answer_correct(self, user_ans, correct_ans):
-        print(correct_ans)
         if user_ans==correct_ans:
             self.user_score+=1
             print(f"You got it right\nIt was {correct_ans}\nYour score: {self.user_score}/{self.question_number}\n")
